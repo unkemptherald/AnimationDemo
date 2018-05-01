a5ca48db7099e23f6d81e1d0523331c0be18c15c
ascott570 <ascott570@PR33.hhs.win.fuhsd.org>
2018-05-01 11:31:44 -0700

    it needs a negative acceleration curve

--- Test/src/Mario.java
+++ Test/src/Mario.java
@@ -9,22 +9,35 @@ public class Mario extends Sprite {
 
 	public static final int MARIO_WIDTH = 40;
 	public static final int MARIO_HEIGHT = 60;
-
+	boolean isInAir = false;
+	double yVel;
 	public Mario(PImage img, int x, int y) {
 		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
 	}
 
 	// METHODS
 	public void walk(int dir) {
+		if(dir>0) 
+		{
+			this.moveByAmount(5, 0);
+		}
+		else 
+		{
+			this.moveByAmount(-5, 0);
+		}
 		// WALK!
 	}
 
 	public void jump() {
-		// JUMP!
+		if(!isInAir) 
+		{
+			yVel +=15;
+		}
 	}
 
-	public void act(ArrayList<Shape> obstacles) {
-		// FALL (and stop when a platform is hit)
+	public void act(ArrayList<Shape> obstacles) 
+	{
+		
 	}
 
 
